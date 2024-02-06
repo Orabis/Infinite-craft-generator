59619a5fbb0313e227eb9f2ba98ae211281c06c2
MERKEL LEO <lmerkel@unistra.fr>
2024-02-06 11:43:42 +0100

    finish inf-craft, printing and writing when new

--- infinite.py
+++ infinite.py
@@ -4,24 +4,33 @@ import time
 
 from function import make_first_requests, make_requests_fusion
 
-unique_texts = set()
-elements = []
 
 soup = make_first_requests()
-print("making first GET, sleeping 1 second..")
 time.sleep(1)
+elements = set()
+unique_texts = set()
+
 for element in soup.find_all("div", class_="item"):
-    text_without_emojis = re.sub(r'^[^\w\s]+', '', element.get_text(strip=True))
-    unique_texts.add(text_without_emojis)
+    text_without_emojis = re.sub(r"^[^\w\s]+", "", element.get_text(strip=True))
+    elements.add(text_without_emojis)
 
-elements = list(unique_texts)
-combinations = product(elements, repeat=len(elements))
+elements = list(elements)
 
-for combination in combinations:
-    result = combination[0]
-    for i in range(1, len(combination)):
-        result = make_requests_fusion(result, combination[i])
-        time.sleep(0.3)
-    print(f"Combination: {combination}, Result: {result}")
-    unique_texts.add(result)
-print(unique_texts)
+while True:
+    for combination in product(elements, repeat=2):
+        value1, value2 = combination
+        result, is_new = make_requests_fusion(value1, value2)
+        if is_new:
+            message = f"New {value1} + {value2} = {result}"
+            print(message)
+            with open("news.txt", "w") as file:
+                file.write(message)
+                file.write("\n")
+        elif result:
+            time.sleep(0.2)
+            unique_texts.add(result)
+            print(f"{value1} + {value2} = {result}")
+    if not unique_texts:
+        break
+    elements.extend(unique_texts)
+print("Test complet.")
