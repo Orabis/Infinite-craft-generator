65945949ca8c768adbd662c538c5da71895a4655
MERKEL LEO <lmerkel@unistra.fr>
2024-02-08 15:36:15 +0100

    refactor all elements

--- function.py
+++ function.py
@@ -1,9 +1,11 @@
+import json
+import re
+import time
 import requests
 from bs4 import BeautifulSoup
-import json
 
-with open("headers.txt", "r") as file:
-    headers_lines = file.readlines()
+with open("headers.txt", "r") as header:
+    headers_lines = header.readlines()
 
 headers = {}
 
@@ -31,3 +33,55 @@ def make_first_requests():
     r = s.get("https://neal.fun/infinite-craft/", headers={"User-Agent": user_agent})
     soup = BeautifulSoup(r.content, "html.parser")
     return soup
+
+
+def check_data_storage():
+    with open("data-storage.json") as data_storage:
+        try:
+            storage_content = json.load(data_storage)
+        except json.decoder.JSONDecodeError:
+            storage_content = None
+    if storage_content is not None:
+        unique_texts = set([text["text"] for text in storage_content["elements"][4:]])
+        elements = [text["text"] for text in storage_content["elements"]]
+        is_data_stored = True
+    else:
+        unique_texts = set()
+        elements = []
+        is_data_stored = False
+    json_template = assign_elements(is_data_stored, elements, storage_content)
+    return unique_texts, elements, is_data_stored, storage_content, json_template
+
+
+def assign_elements(is_data_stored, elements, storage_content):
+    if not is_data_stored:
+        json_template = {
+            "elements": [
+                {"text": "Water", "emoji": "💧", "discovered": False},
+                {"text": "Fire", "emoji": "🔥", "discovered": False},
+                {"text": "Wind", "emoji": "🌬️", "discovered": False},
+                {"text": "Earth", "emoji": "🌍", "discovered": False},
+            ]
+        }
+        for element in make_first_requests().find_all("div", class_="item"):
+            text_without_emojis = re.sub(r"^[^\w\s]+", "", element.get_text(strip=True))
+            if text_without_emojis not in elements:
+                elements.append(text_without_emojis)
+    else:
+        json_template = storage_content
+    return json_template
+
+
+def is_result_new(unique_texts, is_new, result, value1, value2):
+    if is_new:
+        time.sleep(0.2)
+        message = f"New-result {value1} + {value2} = {result}"
+        print(message)
+        with open("news.txt", "a") as file:
+            file.write(message)
+            file.write("\n")
+    elif result:
+        time.sleep(0.2)
+        print(f"{value1} + {value2} = {result}")
+    unique_texts.add(result)
+    return unique_texts
--- infinite.py
+++ infinite.py
@@ -1,46 +1,15 @@
+import time
 import json
 import random
-import re
 from itertools import product
-import time
 
-from function import make_first_requests, make_requests_fusion
+from function import make_requests_fusion, check_data_storage, is_result_new
 
 exit_while = False
+unique_texts, elements, is_data_stored, storage_content, json_template = (
+    check_data_storage()
+)
 
-with open('data-storage.json') as data_storage:
-    try:
-        storage_content = json.load(data_storage)
-    except json.decoder.JSONDecodeError:
-        storage_content = None
-
-if storage_content is not None:
-    unique_texts = set([text['text'] for text in storage_content["elements"][4:]])
-    elements = [text['text'] for text in storage_content["elements"]]
-    is_data_stored = True
-else:
-    unique_texts = set()
-    elements = []
-    is_data_stored = False
-
-if not is_data_stored:
-    json_template = {
-        "elements": [
-            {"text": "Water", "emoji": "💧", "discovered": False},
-            {"text": "Fire", "emoji": "🔥", "discovered": False},
-            {"text": "Wind", "emoji": "🌬️", "discovered": False},
-            {"text": "Earth", "emoji": "🌍", "discovered": False},
-        ]
-    }
-    for element in make_first_requests().find_all("div", class_="item"):
-        text_without_emojis = re.sub(r"^[^\w\s]+", "", element.get_text(strip=True))
-        if text_without_emojis not in elements:
-            elements.append(text_without_emojis)
-else:
-    json_template = storage_content
-"""
-Seems awkward but no need to end the code, theoretically result are infinite
-"""
 
 while not exit_while:
     try:
@@ -48,21 +17,15 @@ while not exit_while:
         for combination in product(elements, repeat=2):
             value1, value2 = combination
             result, is_new, emoji = make_requests_fusion(value1, value2)
-            if is_new:
-                time.sleep(0.2)
-                message = f"New-result {value1} + {value2} = {result}"
-                print(message)
-                with open("news.txt", "a") as file:
-                    file.write(message)
-                    file.write("\n")
-            elif result:
-                time.sleep(0.2)
-                unique_texts.add(result)
-                print(f"{value1} + {value2} = {result}")
-            for text in unique_texts:
-                if text not in elements:
-                    elements.append(text)
-                    new_element = {"text": text, "emoji": emoji, "discovered": is_new}
+            unique_texts = is_result_new(unique_texts, is_new, result, value1, value2)
+            for element in unique_texts:
+                if element not in elements:
+                    elements.append(element)
+                    new_element = {
+                        "text": element,
+                        "emoji": emoji,
+                        "discovered": is_new,
+                    }
                     json_template["elements"].append(new_element)
             if not unique_texts:
                 break
